ba3e545c015943df6c50cebd1be82dece725c073
Domenic <domeniczingsheim@gmail.com>
2021-02-08 17:32:09 +0100

    Add Byte and Word defines

---
+++ src/Core/Defines.h
@@ -0,0 +1,12 @@
+#ifndef __E6502_DEFINES_H
+#define __E6502_DEFINES_H
+
+#include <cstdint>
+
+namespace E6502
+{
+    using Byte = int8_t;    //Byte
+    using Word = int16_t;   //Word = 2 Bytes
+}
+
+#endif
