9ae9e2618b9a84a92c4d3ca8a6fc9f1c477f0370
Domenic <domeniczingsheim@gmail.com>
2021-02-09 00:25:13 +0100

    Add definition of an operator

---
+++ src/Instructions/Instruction.h
@@ -0,0 +1,35 @@
+#ifndef __E6502_INSTRUCTIONS_INSTRUCTION_H
+#define __E6502_INSTRUCTIONS_INSTRUCTION_H
+
+#include <Core/Defines.h>
+#include <Processor/ProcessorStatus.h>
+
+namespace E6502
+{
+    /**
+     *  @brief The base class for instructions
+     */
+    class Instruction
+    {
+        public:
+            /**
+             *  @brief The Instruction operator.
+             *  The derived class has to implement this operator based on the operators definition
+             *  and update registers/pointers/Processor status as defined by the operator
+             *  @param PC The program counter
+             *  @param SP The stack pointer
+             *  @param A The A register
+             *  @param X The X register
+             *  @param Y The Y register
+             *  @param processor_status The processor status
+             */
+            virtual void operator()(Word& PC,                            
+                                    Byte& SP,                         
+                                    Byte& A,                             
+                                    Byte& X,                             
+                                    Byte& Y,                             
+                                    ProcessorStatus& processor_status) = 0;  
+    };
+}
+
+#endif
