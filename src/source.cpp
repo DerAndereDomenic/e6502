c6a6fca7ac17f7eb937807f6491a2f895a4ee7e9
Domenic <domeniczingsheim@gmail.com>
2021-02-08 18:33:09 +0100

    Add some test code

--- src/source.cpp
+++ src/source.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
 #include <Processor/Processor.h>
+#include <Memory/Memory.h>
 
 int main()
 {
+    E6502::Processor procesor;
+    E6502::Memory memory;
+
+    memory[INIT_STACK] = 5;
+
+    std::cout << static_cast<int>(memory[INIT_STACK]) << std::endl;
 
     return 0;
 }
