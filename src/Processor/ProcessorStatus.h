4610b71aac8cb8965954ce361dca5775867d77cc
Domenic <domeniczingsheim@gmail.com>
2021-02-08 17:47:16 +0100

    Add ProcessorStatus struct

---
+++ src/Processor/ProcessorStatus.h
@@ -0,0 +1,20 @@
+#ifndef __E6502_PROCESSOR_PROCESSORSTATUS_H
+#define __E6502_PROCESSOR_PROCESSORSTATUS_H
+
+#include <Core/Defines.h>
+
+namespace E6502
+{
+    struct ProcessorStatus
+    {
+        Byte C : 1;     /**< Carry bit */
+        Byte Z : 1;     /**< Zero bit */
+        Byte I : 1;     /**< Interrupt Disable */
+        Byte D : 1;     /**< Decimal */
+        Byte B : 1;     /**< Break */
+        Byte V : 1;     /**< Overflow */
+        Byte N : 1;     /**< Negative */
+    };
+}
+
+#endif
