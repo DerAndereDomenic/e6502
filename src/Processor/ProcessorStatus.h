753fca5b45fd50bcd28eec71e62bca2b5e103895
Domenic <domeniczingsheim@gmail.com>
2021-02-10 20:23:16 +0100

    Add definitions for bit checks

--- src/Processor/ProcessorStatus.h
+++ src/Processor/ProcessorStatus.h
@@ -7,6 +7,42 @@ namespace E6502
 {
     struct ProcessorStatus
     {
+        /**
+         *  @brief Check if the given byte is zero.
+         *  If the byte is zero, the zero bit is set to 1, 0 otherwise
+         *  @param byte The byte to check
+         */
+        void 
+        checkZero(const Byte& byte);
+
+        /**
+         *  @brief Check if the carry bit is set.
+         *  The calculation should be converted to a Word (16 bit).
+         *  This way we can check if the input is out of the range [0,255].
+         *  Sets the carry bit 1 if words 9th bit is set, 0 otherwise.
+         *  @param word The word to check
+         */
+        void 
+        checkCarry(const Word& word);
+
+        /**
+         *  @brief Check if overflow was encountered.
+         *  The calculation should be converted to a Word (16 bit).
+         *  This way we can check if the input is out of the range [-127,128].
+         *  Sets the overflow bit 1 if overflow was encountered, 0 else
+         *  @param word The word to check
+         */
+        void 
+        checkOverflow(const Word& word);
+
+        /**
+         *  @brief Check if the byte is negative.
+         *  The negative bit gets set to the 8th bit.
+         *  @param byte The byte to check. 
+         */
+        void 
+        checkNegative(const Byte& byte);
+
         Byte C : 1;     /**< Carry bit */
         Byte Z : 1;     /**< Zero bit */
         Byte I : 1;     /**< Interrupt Disable */
