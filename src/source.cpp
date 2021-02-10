199b59d78648fc6a51b5bae203d06e0f4574ac1e
Domenic <domeniczingsheim@gmail.com>
2021-02-10 23:31:27 +0100

    Implement short test program and print execution time

--- src/source.cpp
+++ src/source.cpp
@@ -1,17 +1,23 @@
 #include <iostream>
 #include <Processor/Processor.h>
 #include <Memory/Memory.h>
+#include <chrono>
 
 int main()
 {
     E6502::Memory memory;
+    memory[STACK_START + 1] = E6502::SED;
+    memory[STACK_START + 2] = E6502::SEC;
+    memory[STACK_START + 3] = E6502::CLD;
     E6502::Processor processor(memory);
 
+    auto start_time = std::chrono::high_resolution_clock::now();
     processor.start();
+    auto end_time = std::chrono::high_resolution_clock::now();
 
     processor.printProcessorStatus();
 
-    E6502::ProcessorStatus status;
+    std::cout << "Execution time: " << (end_time-start_time)/std::chrono::milliseconds(1) << " ms" << std::endl;
 
     return 0;
 }
