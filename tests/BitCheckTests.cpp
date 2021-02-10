b52fa394afecc4f123d541d0ab9d5a8a9e656950
Domenic <domeniczingsheim@gmail.com>
2021-02-10 21:00:50 +0100

    Add tests for negative flag

--- tests/BitCheckTests.cpp
+++ tests/BitCheckTests.cpp
@@ -29,3 +29,48 @@ TEST(zeroFlagNegative, zeroFlagNegative)
 
     EXPECT_EQ(status.Z, 0);
 }
+
+TEST(negativeFlagPositive, negativeFlagPositive)
+{
+    ProcessorStatus status;
+    Byte byte = 10;
+    status.checkNegative(byte);
+
+    EXPECT_EQ(status.N,0);
+}
+
+TEST(negativeFlagNegative, negativeFlagNegative)
+{
+    ProcessorStatus status;
+    Byte byte = -10;
+    status.checkNegative(byte);
+
+    EXPECT_EQ(status.N,1);
+}
+
+TEST(negativeFlagZero, negativeFlagZero)
+{
+    ProcessorStatus status;
+    Byte byte = 0;
+    status.checkNegative(byte);
+
+    EXPECT_EQ(status.N,0);
+}
+
+TEST(negativeFlagOverflow, negativeFlagOverflow)
+{
+    ProcessorStatus status;
+    Byte byte = 128;
+    status.checkNegative(byte);
+
+    EXPECT_EQ(status.N,1);
+}
+
+TEST(negativeFlagUnderflow, negativeFlagUnderflow)
+{
+    ProcessorStatus status;
+    int16_t byte = -129;    //Trick compiler to not display overflow warning
+    status.checkNegative(static_cast<uint8_t>(byte));
+
+    EXPECT_EQ(status.N,0);
+}
