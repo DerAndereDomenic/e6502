f4086758df975d06018246d81afa90c6ac490866
Domenic <domeniczingsheim@gmail.com>
2021-02-11 00:13:31 +0100

    Add test program for LDA

--- src/source.cpp
+++ src/source.cpp
@@ -6,9 +6,14 @@
 int main()
 {
     E6502::Memory memory;
-    memory[STACK_START + 1] = E6502::SED;
-    memory[STACK_START + 2] = E6502::SEC;
-    memory[STACK_START + 3] = E6502::CLD;
+    memory[STACK_START + 1] = E6502::LDA_I;
+    memory[STACK_START + 2] = E6502::LDA_Z;
+    memory[STACK_START + 3] = E6502::LDA_ZX;
+    memory[STACK_START + 4] = E6502::LDA_A;
+    memory[STACK_START + 5] = E6502::LDA_AX;
+    memory[STACK_START + 6] = E6502::LDA_AY;
+    memory[STACK_START + 7] = E6502::LDA_IX;
+    memory[STACK_START + 8] = E6502::LDA_IY;
     E6502::Processor processor(memory);
 
     auto start_time = std::chrono::high_resolution_clock::now();
