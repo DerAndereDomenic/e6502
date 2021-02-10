72a126efc395ad4c8136dcc7fc09f2821ae7052f
Domenic <domeniczingsheim@gmail.com>
2021-02-11 00:22:28 +0100

    Implement immediate loading

--- src/Instructions/LDA.cpp
+++ src/Instructions/LDA.cpp
@@ -1,4 +1,5 @@
 #include <Instructions/LDA.h>
+#include <iostream>
 
 using namespace E6502_Instructions;
 
@@ -12,5 +13,51 @@ LDA::operator()(Word& PC,
                 Memory& memory,
                 AddressingModes& address_mode)
 {
-    
+    switch(address_mode)
+    {
+        case ZERO_PAGE_X_INDIRECT_01:
+        {
+            std::cout << "LDA: Zero Page X Indirect" << std::endl;
+        }
+        break;
+        case ZERO_PAGE_01:
+        {
+            std::cout << "LDA: Zero Page" << std::endl;
+        }
+        break;
+        case IMMEDIATE_01:
+        {
+            Byte data = memory[PC];
+            A = data;
+            processor_status.checkZero(data);
+            processor_status.checkNegative(data);
+            ++PC;
+        }
+        break;
+        case ABSOLUTE_01:
+        {
+            std::cout << "LDA: Absolute" << std::endl;
+        }
+        break;
+        case ZERO_PAGE_Y_INDIRECT_01:
+        {
+            std::cout << "LDA: Zero Page Y Indirect" << std::endl;
+        }
+        break;
+        case ZERO_PAGE_X_01:
+        {
+            std::cout << "LDA: Zero Page X" << std::endl;
+        }
+        break;
+        case ABSOLUTE_Y_01:
+        {
+            std::cout << "LDA: Absolute Y" << std::endl;
+        }
+        break;
+        case ABSOLUTE_X_01:
+        {
+            std::cout << "LDA: Absolute X" << std::endl;
+        }
+        break;
+    }
 }
