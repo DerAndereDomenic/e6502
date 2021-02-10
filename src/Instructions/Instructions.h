16eae80dc8329caf5e7e141e388ed868604b196d
Domenic <domeniczingsheim@gmail.com>
2021-02-10 23:16:02 +0100

    Add instruction includes

--- src/Instructions/Instructions.h
+++ src/Instructions/Instructions.h
@@ -1,4 +1,12 @@
 #ifndef __E6502_INSTRUCTIONS_INSTRUCTIONS_H
 #define __E6502_INSTRUCTIONS_INSTRUCTIONS_H
 
+#include <Instructions/CLC.h>
+#include <Instructions/SEC.h>
+#include <Instructions/CLI.h>
+#include <Instructions/SEI.h>
+#include <Instructions/CLV.h>
+#include <Instructions/CLD.h>
+#include <Instructions/SED.h>
+
 #endif
